49db861e02681d6d6091ae25c2594c03c85a8e04
dg-data <66296000+dg-data@users.noreply.github.com>
2021-03-16 08:45:41 +0100

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,3 @@
+$('#appmode-leave').hide();                          // Hides the edit app button
+$('#appmode-busy').hide();                           // Hides the kernel busy
+$('#appmode-loader').append('<h2>Loading...</h2>');
